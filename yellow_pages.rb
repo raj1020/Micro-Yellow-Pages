087211617b0a17c84700e9af419613080d76691c
raj1020 <raj.1020@hotmail.com>
2019-10-02 21:14:21 +1000

    created a micro-yellowpages in ruby file

---
+++ yellow_pages.rb
@@ -0,0 +1,22 @@
+
+yellow_pages=[
+
+                 {name: "Alex Holder",
+                 tel: +6100000000,
+                 designation: "Educator at Coder Academy",
+                 address: "118 Walker St, North Sydney, NSW 2060"
+                    },
+
+                    {name: "Ubisoft Australia",
+                 tel: +6100000001,
+                 designation: "Videogame sales & support",
+                 address: "14 Mountain St, Ultimo, NSW 2007"
+                 },
+
+                 {name: "Saad Saaed",
+                 tel: +6100000002,
+                 designation: "Academic & Partnerships Director at Coder Academy",
+                 address: "118 Walker St, North Sydney, NSW 2060"
+                 }
+               ]
+
