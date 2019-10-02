fd724bb617446b86c32a852fe5a854e36bc6e1c3
raj1020 <raj.1020@hotmail.com>
2019-10-02 21:56:05 +1000

    added method, loop and if condition

--- yellow_pages.rb
+++ yellow_pages.rb
@@ -3,20 +3,57 @@ yellow_pages=[
 
                  {name: "Alex Holder",
                  tel: +6100000000,
-                 designation: "Educator at Coder Academy",
+                 role: "Educator at Coder Academy",
                  address: "118 Walker St, North Sydney, NSW 2060"
                     },
 
                     {name: "Ubisoft Australia",
                  tel: +6100000001,
-                 designation: "Videogame sales & support",
+                 role: "Videogame sales & support",
                  address: "14 Mountain St, Ultimo, NSW 2007"
                  },
 
                  {name: "Saad Saaed",
                  tel: +6100000002,
-                 designation: "Academic & Partnerships Director at Coder Academy",
+                 role: "Academic & Partnerships Director at Coder Academy",
                  address: "118 Walker St, North Sydney, NSW 2060"
                  }
                ]
+def choice
+    puts "What do you want to do?"
+    p "1. View Entries"
+    p "2. Add Entries"
+    p "3. Remove Entries" 
+    p "Please enter the option number of your choice."
+    choice_number=gets.chomp.to_i
+    return choice_number
+end
 
+choice number=choice
+i=0
+
+while choice_number<1 || choice_number>3
+    puts "Invalid Choice. Please choose again"
+    choice_number=choice
+    i+=1
+    if i>=3
+        puts"Too many wrong entries. Please try again later."
+        break
+    end
+
+
+end
+
+if choice_number==1
+    p yellow_pages
+
+end
+if choice_number==2
+
+
+end
+
+puts "What is the name?"
+name=gets.chomp
+puts "What is the number?"
+number= gets.chomp
